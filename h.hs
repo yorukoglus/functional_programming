67f6643fce5eda78be4d47c0d684a59fddd1eb56
yorukoglus <36706838+yorukoglus@users.noreply.github.com>
2018-05-21 00:31:50 +0300

    insert list implemented with foldr and search function implemented

--- h.hs
+++ h.hs
@@ -13,19 +13,36 @@ empty = Trie {end = False, children = M.empty}
 insert :: Word -> Trie -> Trie
 insert []     t = t {end = True}
 insert (c:cs) t = case M.lookup c ts of
-                   Just t' -> t { children = M.insert c (insert cs t') ts }
-                   Nothing -> t { children = M.insert c (insert cs child_node) (M.insert c child_node ts) }
+                   Just t' -> t { end = False , children = M.insert c (insert cs t') ts }
+                   Nothing -> t { children = M.insert c (insert cs child_node) (M.insert c child_node ts)}
                    where
                     ts = children t
                     child_node = empty
 
 insertList :: [Word] -> Trie
-insertList = undefined
+insertList ws = foldr insert empty ws
+
 search :: Word -> Trie -> Bool
-search = undefined
+search (c:[]) t = case M.lookup c ts of
+                   Nothing -> False
+                   Just t' -> (if (end t'==True) then True else False)
+                   where
+                    ts = children t
+search (c:cs) t = case M.lookup c ts of
+                   Nothing -> False
+                   Just t' -> search cs t'
+                   where
+                    ts = children t
+                   
+                  
+
 getWords :: Trie -> [Word]
 getWords = undefined
 prefix :: Word -> Trie -> Maybe [Word]
 prefix = undefined
 
 
+trie = insert "a" empty
+y = insert "b" trie
+z = insert "c" y
+r = insertList ["abla","baba","abi","kardes","abiye"]
