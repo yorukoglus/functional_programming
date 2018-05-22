83e40687fa4553c0a605f034519d69adf313fc2b
yorukoglus <36706838+yorukoglus@users.noreply.github.com>
2018-05-22 15:51:32 +0300

    menu operations added

--- h.hs
+++ h.hs
@@ -1,10 +1,11 @@
 import qualified Data.Map as M --(can be shortened)
+import qualified Data.Foldable as F
 import Data.Maybe
 import System.Environment
 import System.IO
 import Prelude hiding (Word)
 data Trie = Trie {end :: Bool, children :: M.Map Char Trie}
-            deriving (Show)
+            deriving (Show,Read)
 type Word = String
 
 empty :: Trie
@@ -23,7 +24,7 @@ insertList :: [Word] -> Trie
 insertList ws = foldr insert empty ws
 
 search :: Word -> Trie -> Bool
-search (c:[]) t = case M.lookup c ts of
+search [c]    t = case M.lookup c ts of
                    Nothing -> False
                    Just t' -> (if (end t'==True) then True else False)
                    where
@@ -33,16 +34,72 @@ search (c:cs) t = case M.lookup c ts of
                    Just t' -> search cs t'
                    where
                     ts = children t
-                   
-                  
-
-getWords :: Trie -> [Word]
-getWords = undefined
-prefix :: Word -> Trie -> Maybe [Word]
-prefix = undefined
 
 
-trie = insert "a" empty
-y = insert "b" trie
-z = insert "c" y
-r = insertList ["abla","baba","abi","kardes","abiye"]
+keyList :: [(k,l)] -> [k]
+keyList ts = fst((unzip ts))
+
+
+getWords :: Trie -> Word
+getWords t = concat $ getIter t []
+
+  
+getIter :: Trie -> [Word] -> [Word]
+getIter t ws = case M.lookup c ts of 
+ Just t' -> if (end t'==True) then [[c]]++ws else (getIter t' [[c]]++ws)
+ Nothing -> ws
+ where
+  ts = children t
+  l@(c:cs)=keyList (M.toList ts)
+
+find_prefix = undefined
+  
+print_trie :: Trie -> IO ()
+print_trie t = print $ M.showTree (children t)
+
+getInput :: IO String
+getInput = do inp <- getLine
+              return inp
+
+main :: IO ()
+main = do 
+    args <- getArgs
+    let file_name = head args
+    handle <- openFile file_name ReadMode
+    contents <- hGetContents handle
+    let t = insertList $ lines contents
+    mainloop t
+    hClose handle
+
+mainloop :: Trie -> IO ()
+mainloop in_t = 
+ do 
+    putStrLn "a) Add Word"
+    putStrLn "s) Search Word"
+    putStrLn "f) Find words with prefix"
+    putStrLn "p) Print all words"
+    putStrLn "e) Exit\nEnter the action:"
+    c <- getLine
+    case c of
+     "e" -> return ()
+     "s" -> do
+         putStrLn "Enter word/prefix:"
+         str <- getLine
+         if search str in_t then do 
+          putStrLn "Exists in dictionary!\n------------------------------"
+          mainloop in_t
+         else do 
+          putStrLn "NOT exist!\n------------------------------"
+          mainloop in_t
+     "a" -> do 
+         putStrLn "Enter word/prefix:"
+         str <- getLine
+         let new_t = insert str in_t
+         putStrLn "New word is added!\n------------------------------"
+         mainloop new_t
+     "f" -> find_prefix
+     "p" -> print_trie in_t
+     otherwise -> do 
+         putStrLn "Input is incorrect!\n------------------------------"
+         mainloop in_t
+    
